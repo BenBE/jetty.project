368ac7d62835bb4a6256300d9afe49f97ba9f426
Jan Bartel <janb@intalio.com>
2014-02-17 15:16:44 +1100

    Make a Jetty specific servletcontainer initializer that pushes pre-found tlds into jasper.

---
+++ apache-jsp/src/main/java/org/mortbay/apache/jsp/JettyJasperInitializer.java
@@ -0,0 +1,56 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2014 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+package org.mortbay.apache.jsp;
+
+import java.net.URL;
+import java.util.Collection;
+
+import javax.servlet.ServletContext;
+
+import org.apache.jasper.servlet.JasperInitializer;
+import org.apache.jasper.servlet.TldScanner;
+
+/**
+ * JettyJasperInitializer
+ *
+ */
+public class JettyJasperInitializer extends JasperInitializer
+{
+
+    /**
+     * Make a TldScanner, and prefeed it the tlds that have already been discovered in jar files
+     * by the MetaInfConfiguration.
+     * 
+     * @see org.apache.jasper.servlet.JasperInitializer#prepareScanner(javax.servlet.ServletContext, boolean, boolean, boolean)
+     */
+    @Override
+    public TldScanner prepareScanner(ServletContext context, boolean namespaceAware, boolean validate, boolean blockExternal)
+    {
+        TldScanner scanner = super.prepareScanner(context, namespaceAware, validate, blockExternal);
+        Collection<URL> tldUrls = (Collection<URL>)context.getAttribute("org.eclipse.jetty.tlds");
+        if (tldUrls != null && !tldUrls.isEmpty())
+        {
+System.err.println("Set org.eclipse.jetty.tlds to "+tldUrls.size());
+            scanner.setJarTldURLs(tldUrls);
+System.err.println("After call to scan");
+        }
+        return scanner;
+    }
+    
+
+}
