fd7f8ab5732f95ad3afabebf897c333370da90f5
Benny Baumann <BenBE@geshi.org>
2014-07-13 19:25:33 +0200

    Fix potential timing issue when comparing passwords
    We now only leak the information if a password was compared or not (because of digest unavailability)
    
    The previous version additionally leaked how much of the password hashes matched and thus might allow for the hash being reconstructed.

--- jetty-util/src/main/java/org/eclipse/jetty/util/security/Credential.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/security/Credential.java
@@ -146,31 +146,33 @@ public abstract class Credential implements Serializable
         {
             try
             {
-                byte[] digest = null;
-
-                if (credentials instanceof char[])
+                if (credentials instanceof char[]) {
                     credentials=new String((char[])credentials);
-                if (credentials instanceof Password || credentials instanceof String)
-                {
-                    synchronized (__md5Lock)
-                    {
-                        if (__md == null) __md = MessageDigest.getInstance("MD5");
-                        __md.reset();
-                        __md.update(credentials.toString().getBytes(StandardCharsets.ISO_8859_1));
-                        digest = __md.digest();
-                    }
-                    if (digest == null || digest.length != _digest.length) return false;
-                    for (int i = 0; i < digest.length; i++)
-                        if (digest[i] != _digest[i]) return false;
-                    return true;
                 }
-                else if (credentials instanceof MD5)
+
+                if (credentials instanceof MD5 || credentials instanceof Password || credentials instanceof String)
                 {
-                    MD5 md5 = (MD5) credentials;
-                    if (_digest.length != md5._digest.length) return false;
-                    for (int i = 0; i < _digest.length; i++)
-                        if (_digest[i] != md5._digest[i]) return false;
-                    return true;
+                    byte[] digest = null;
+
+                    if (credentials instanceof MD5) {
+                        MD5 md5 = (MD5) credentials;
+                        digest = md5._digest;
+                    } else {
+                        synchronized (__md5Lock)
+                        {
+                            if (__md == null) __md = MessageDigest.getInstance("MD5");
+                            __md.reset();
+                            __md.update(credentials.toString().getBytes(StandardCharsets.UTF_8));
+                            digest = __md.digest();
+                        }
+                    }
+
+                    if (digest == null || digest.length != _digest.length) return false;
+
+                    bool digestMismatch = false;
+                    for (int i = 0; i < digest.length; i++)
+                        digestMismatch |= (digest[i] != _digest[i]);
+                    return !digestMismatch;
                 }
                 else if (credentials instanceof Credential)
                 {
